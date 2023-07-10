acc948f5d7aad78306d0b403b3edda8f685bb146
isurumnaod <isurumanod99@gmail.com>
2023-07-10 06:48:03 +0530

    Add tailwind css to skill component

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -1,16 +1,20 @@
-import React from 'react';
-import "../styles/Skill.css"
-function Skill({img,name}) {
+import {motion} from "framer-motion"
+
+
+function Skill({name,img,directionLeft}) {
     return (
-        <div className="skill">
-             <div className="skills__container__row__bar">
-                            <div className="skills__container__row__bar__info">
-                                <img className="skills__container__row__bar__info__image" src={img}
-                                     alt="skill"/>
-                                <span className="skills__container__row__bar__info__image-title">{name}</span>
-                            </div>
-                        </div>
+        <div className={"group relative flex cursor-pointer "}>
+            <motion.img
+                initial={{x: directionLeft ? -200 : 200, opacity: 0}}
+                whileInView={{opacity: 1, x: 0}}
+                transition={{duration: 1}}
+
+                src={img}
+                className={"w-16 h-16 md:w-20 md:h-20 lg:w-24 lg:h-24 rounded-full object-cover border border-gray-500  p-2"}
+            />
+
         </div>
+
     );
 }
 
